e72190f74169deff2edd1e09c28e0fee111a8e25
akshaymittur <akshaymittur@gmail.com>
2020-03-10 18:06:45 +0530

    changes

--- main.py
+++ main.py
@@ -239,6 +239,14 @@ def run(config_path):
                     neat.DefaultSpeciesSet, neat.DefaultStagnation,
                     config_file)
 
+    p = neat.Population(config)
+
+    p.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    p.add_reporter(stats)
+
+    winner = p.run(main, 50)
+
 if __name__ == '__main__':
     local_dir = os.path.dirname(__file__)
     config_path = os.path.join(local_dir, "config.txt")
